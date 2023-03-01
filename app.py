649f25a788ba74cee00468a1469c76326c1514b1
Bharath kumar <82494037+bharathkumar192@users.noreply.github.com>
2023-03-02 00:17:00 +0530

    app changes

--- app.py
+++ app.py
@@ -2,54 +2,28 @@ import time
 from absl import app, logging
 import cv2
 import numpy as np
-from flask import Flask, request, Response, jsonify, send_from_directory, abort, render_template
+from flask import Flask, request, Response, jsonify, send_from_directory, abort, render_template,send_file
 import os
 import sys
 from subprocess import call
-from detect import detect
+from detect import *
 
 
-def run_cmd(command):
-        try:
-            call(command, shell=True)
-        except KeyboardInterrupt:
-            print("Process interrupted")
-            sys.exit(1)
-
-# Initialize Flask application
 app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return 'hello world'
 
-# API that returns image with detections on it
+
 @app.route('/image', methods= ['POST'])
 def get_image():
-    image = request.files["images"]
-    image_name = image.filename
-    image.save(os.path.join(os.getcwd(), image_name))
-
-    t1 = time.time()
-    img=detect(image_name)
-    t2 = time.time()
-    print('time: {}'.format(t2 - t1))
-
-    print('detections:')
-    cv2.imwrite('results.png', img)
-    print('output saved to: {}'.format('results.png'))
-    
-    # prepare image for response
-    _, img_encoded = cv2.imencode('.png', img)
-    response = img_encoded.tostring()
-    
-    #remove temporary image
-    os.remove(image_name)
-
-    try:
-        return Response(response=response, status=200, mimetype='image/png')
-    except FileNotFoundError:
-        abort(404)
+   image = request.files['image']
+   image.save(image.filename)
+   img = detect(image.filename)
+   cv2.imwrite('results.png', img)
+   os.remove(image.filename)
+   return send_file('results.png', mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug=True, host = '0.0.0.0', port=5000)
