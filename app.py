dae04d843ec9419915f7f0fab350d0e218fa3cc0
Bharath kumar <82494037+bharathkumar192@users.noreply.github.com>
2023-03-02 11:36:01 +0530

    cors added

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ import os
 import sys
 from subprocess import call
 from detect import *
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
+
+
 @app.route('/')
 def index():
    print('Request for index page received')
